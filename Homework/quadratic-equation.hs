f769b579e608bff062aa7ff98a5b52b99cbb648d
Artem <artemtarasov678@gmail.com>
2019-10-21 18:12:39 +0700

    homework 1 was fixed, exceptions was added in function, foldl foldr (+++) was implemented, length was implemented by tail recursion

--- Homework/quadratic-equation.hs
+++ Homework/quadratic-equation.hs
@@ -1,5 +1,6 @@
-quadraticRoots :: Float -> Float -> Float -> (Float, Float)
-quadraticRoots a b c | a == 0 = error "a koefficent shouldn\'t be a zero"
+quadraticRoots :: (Eq a, Floating a) => a -> a -> a -> (a, a)
+quadraticRoots a b c | (a == 0) && (b /= 0) = ((-c / b), (-c / b))
+                     | (a == 0) && (b == 0) = error "this is not equation at all"
                      | otherwise = (x1, x2) where
                         d = b*b - 4 * a * c
                         x1 = (-b + sqrt d) / 2
--- Labs/lists.hs
+++ Labs/lists.hs
@@ -1,44 +1,70 @@
-get' :: (Eq b, Num b) => [a] -> b -> a
+
+get' ::  [a] -> Integer -> a
+get' [] n = error "index out of range"
 get' (x:xs) 0 = x 
-get' (x:xs) n = get' xs (n-1)   
+get' (x:xs) n | n >= 0 = get' xs (n-1)
+              | otherwise = error "index is negative"
 
 head' :: [a] -> a
+head' [] = error "list is empty"
 head' (x:xs) = x 
 
 last' :: [a] -> a 
+last' [] = error "list is empty"
 last' [x] = x
 last' (x:xs) = last' xs
 
-tail' :: [a] -> [a]
-tail' (x:xs) = xs
 
-init' :: [a] -> [a]
+tail' :: [a] -> [a]
+tail' [] = error "list is empty"
+tail' (x:xs) = xs
+ 
+init' :: [a] -> [a] 
+init' [] = error "list is empty"
 init' [x] = []
 init' (x:xs) = x:init' xs
 
-reverse' :: [a] -> [a]
-reverse' xs = foldl (\acc elem -> elem:acc) [] xs -- почему если убрать xs слева и справа, то не произойдет каррирование и будет ошибка
+foldl' ::(a -> b -> a) -> a -> [b] -> a
+foldl' func acc xs = helper acc xs where
+    helper acc [] = acc
+    helper acc (x:xs) = helper (func acc x) xs
 
-length' :: [a] -> Int
+reverse' :: [a] -> [a]
+reverse' = foldl' (\acc elem -> elem:acc) [] 
+
+foldr' :: (b -> a -> a) -> a -> [b] -> a 
+foldr' func acc xs = foldl' (\x y -> func y x) acc (reverse' xs)    
+
+(+++) :: [a] -> [a] -> [a]
+[] +++ xs = xs
+(x:xs) +++ ys = x: (xs +++ ys)
+
+length' :: [a] -> Integer
 length' [] = 0
-length' (x:xs) = 1 + length' xs
+length' xs = helper 0 xs where
+        helper acc [] = 0
+        helper acc (x:xs) = 1 + helper acc xs 
 
 append' :: [a] -> a -> [a]
-append' xs x = xs ++ [x]
+append' xs x = xs +++ [x] 
 
 concat' :: [a] -> [a] -> [a]
-concat' xs1 xs2 = xs1 ++ xs2
+concat' xs1 xs2 = foldr' (\elem acc -> elem:acc) xs2 xs1
 
-drop' :: Int -> [a] -> [a]
+drop' :: Integer -> [a] -> [a]
+drop' n [] = []
 drop' 0 xs = xs
 drop' n (x:xs) = drop' (n - 1) xs
 
-take' :: Int -> [a] -> [a]
+
+take' :: Integer -> [a] -> [a]
+take' n [] = []
 take' 0 xs = []
 take' n (x:xs) = x:take' (n-1) xs
 
-split' :: Int -> [a] -> ([a],[a])
-split' n xs = (take' n xs, drop' n xs)
+split' :: Integer -> [a] -> ([a],[a])
+split' n xs | n <= length' xs && n > 0 = (take' n xs, drop' n xs)
+            | otherwise = error "index is incorrect"
 
 null' :: [a] -> Bool 
 null' [] = True
@@ -49,10 +75,10 @@ elem' [] element = False
 elem' (x:xs) element = if x == element then True else elem' xs element
 
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' test xs = foldl (\ acc elem -> if test elem then elem:acc else acc) [] xs
+filter' test xs = foldr' (\ elem acc -> if test elem then elem:acc else acc) [] xs
 
 map':: (a -> b) -> [a] -> [b]
-map' func xs = foldr (\b local_xs-> func b:local_xs) [] xs
+map' func xs = foldr' (\b local_xs-> func b:local_xs) [] xs
 
 zip' :: [a] -> [b] -> [(a,b)] 
 zip' xs [] = []
