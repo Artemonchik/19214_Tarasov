53691113de5e4fd31ad8d1103982016daf96da95
Artemonchik <33481844+Artemonchik@users.noreply.github.com>
2019-10-22 22:28:17 +0700

    QE. was fixed discriminant calculus

--- Homework/quadratic-equation.hs
+++ Homework/quadratic-equation.hs
@@ -4,8 +4,8 @@ quadraticRoots a b c | (a == 0) && (b /= 0) = ((-c / b), (-c / b))
                      | d < 0 = error "this equation doesn't have roots"
                      | otherwise = (x1, x2) where
                         d = b*b - 4 * a * c
-                        x1 = (-b + sqrt d) / 2
-                        x2 = (-b - sqrt d) / 2
+                        x1 = (-b + sqrt d) / 2 / a
+                        x2 = (-b - sqrt d) / 2 / a 
                         
 
  
