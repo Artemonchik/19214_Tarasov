d5baa81b69b7cb58dee5d4f16a49b818b2bcb8d9
Artem <artemtarasov678@gmail.com>
2019-10-28 17:47:04 +0700

    Quadratic equation was fixed

--- Homework/quadratic-equation.hs
+++ Homework/quadratic-equation.hs
@@ -1,7 +1,7 @@
-quadraticRoots :: (Eq a, Floating a) => a -> a -> a -> (a, a)
-quadraticRoots a b c | (a == 0) && (b /= 0) = ((-c / b), (-c / b))
-                     | (a == 0) && (b == 0) = error "this is not equation at all"
-                     | d < 0 = error "this equation doesn't have roots"
+quadraticRoots :: (Eq a, Floating a, Ord a) => a -> a -> a -> (a, a)
+quadraticRoots 0 0 _ = error "this is not equation at all"
+quadraticRoots 0 b c = ((-c / b), (-c / b))
+quadraticRoots a b c | d < 0 = error "this equation doesn't have roots"
                      | otherwise = (x1, x2) where
                         d = b*b - 4 * a * c
                         x1 = (-b + sqrt d) / 2 / a
