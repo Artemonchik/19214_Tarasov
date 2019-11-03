780501264749c3a1250132d00035a3440633ea77
Artem <artemtarasov678@gmail.com>
2019-11-03 19:50:54 +0700

    lab1 was fixed

--- Labs/numbers.hs
+++ Labs/numbers.hs
@@ -2,39 +2,38 @@ import Data.Char
 import Data.List
 --Переводит строковое представление snumber из системы по основанию base в строковое представление числа в десятичной системе
 getIntegerFromLetter :: Char -> Integer
-getIntegerFromLetter char | (ord char) >= 97 && (ord char) <= 122 = toInteger $ ord char - 87
-                      | ord char >= 48 && (ord char) <= 57 = toInteger $ ord char - 48
-                      | (ord char) >= 65 && (ord char) <= 90 = toInteger $ ord char - 29
+getIntegerFromLetter char | (ord char) >= ord 'a' && (ord char) <= ord 'z' = toInteger $ ord char - ord 'a' + 10
+                      | ord char >= ord '0' && (ord char) <= ord '9' = toInteger $ ord char - ord '0'
+                      | (ord char) >= ord 'A' && (ord char) <= ord 'Z' = toInteger $ ord char - ord 'A' + 36
                       | otherwise = error "this is invalid char"
 
 
 getLetterFromInteger :: Integer -> Char
-getLetterFromInteger integer | integer >= 0 && integer <= 9 = chr (fromIntegral integer + 48)
-                             | integer >= 10 && integer <= 35 =  chr ( fromIntegral integer + 87)
-                             | integer >= 36  && integer <= 61 =  chr (fromIntegral integer + 29)
+getLetterFromInteger integer | integer >= 0 && integer <= 9 = chr (fromIntegral integer + ord '0')
+                             | integer >= 10 && integer <= 35 =  chr ( fromIntegral integer + ord 'a' - 10)
+                             | integer >= 36  && integer <= 61 =  chr (fromIntegral integer + ord 'A' - 36)
                              | otherwise = error "this integer can\'t be represented as char"
 
-toDecimal :: Integer -> String -> String 
-toDecimal base snumber | base >= 1 && base <= 61 = show (answer intArr)
+toDecimal :: Integer -> String -> String
+toDecimal 1 snumber | all (=='1') snumber = show $ length snumber 
+                    | otherwise = error "invalid characters for this base"
+toDecimal base snumber | base >= 1 && base <= 61 = show (answer intArr $ length intArr)
                        | otherwise = error "base is invalid"
                           where intArr = map getIntegerFromLetter snumber 
-                                answer [] = 0  
-                                answer (x:intArr)  | x == 0 && base == 1 = error "char is not valid for this base" 
-                                                   | x == 1 && base == 1 =  x * (base ^ (toInteger $ length intArr)) + answer intArr 
-                                                   | x < base = x * (base ^ (toInteger $ length intArr)) + answer intArr 
+                                answer [] _ = 0  
+                                answer (x:intArr) power | x < base = x * (base ^ (power - 1)) + answer intArr (power - 1)
                                                    | otherwise =  error "char is not valid for this base"
 
-fromDecimal::Integer -> String -> String 
+fromDecimal::Integer -> String -> String
+fromDecimal 1 snumber | any (\char -> ord char < 48 || ord char > 57) snumber = error "incorrect number"
+                      | otherwise = replicate (read snumber) '1'
 fromDecimal toBase snumber | any (\char -> ord char < 48 || ord char > 57) snumber = error "incorrect number"
-                           | toBase <= 61 && toBase > 1 = reverse $ map getLetterFromInteger (answer number)
-                           | toBase == 1 = reverse $ map getLetterFromInteger (answerForBase1 number)
+                           | toBase <= 61 && toBase > 1 = map getLetterFromInteger (answer number [])
                            | otherwise = error "invalid base"
-                    where 
-                        number = read snumber
-                        answerForBase1 0 = []
-                        answerForBase1 n = 1:(answerForBase1 (n - 1))
-                        answer 0 = []
-                        answer number = number `mod` toBase:(answer $ number `div` toBase) 
-
+                            where 
+                                number = read snumber
+                                answer 0 acc = acc
+                                answer number acc = (answer  (number `div` toBase) (number `mod` toBase : acc))    
+                                
 convertFromTo :: Integer -> Integer -> String -> String
 convertFromTo fromBase toBase snumber = fromDecimal toBase $ toDecimal fromBase snumber 
